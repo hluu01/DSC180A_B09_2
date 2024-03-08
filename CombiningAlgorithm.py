0ee8214247e26b75e156076162a992dd0a4a0fff
LohitG <lohit.geddam@gmail.com>
2024-03-08 10:00:24 -0800

    Created Combining algorithm
    Pass in the AutoML response as a list of tuples or lists

---
+++ CombiningAlgorithm.py
@@ -0,0 +1,17 @@
+scaling_dict = {"pants-fire": 0, "false": 20, "barely-true": 40, "half-true": 60, "mostly-true": 80, "true": 100}
+
+def scale_and_combine(truthfullness, automl):
+    average_score = 0
+    for label, score in automl:
+        average_score += scaling_dict[label] * score
+    
+    bounded_score = max(0.25, min(average_score, 0.75))
+    normalized_score = (bounded_score - 0.25) / (0.75 - 0.25)
+    scaled_variable = normalized_score * 100
+
+    if abs(truthfullness - scaled_variable) > 50:
+        return truthfullness * .90 + scaled_variable * .10
+    elif abs(truthfullness - scaled_variable) > 25:
+        return truthfullness * .75 + scaled_variable * .25
+    else:
+        return truthfullness * .60 + scaled_variable * .40
